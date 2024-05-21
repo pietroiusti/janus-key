8db0dc09b2926f29aaa28b749f96596bcc1a1ac9
gp <giulio.pietroiusti@gmail.com>
2024-05-21 23:38:58 +0200

    add ``delayed down'' functionality
    This commit makes it the case that, if a janus key is held down for
    more than the max delay specified in the config file, then its 2nd
    function is sent down.
    
    The main reason why I wanted to implement this feature was that I
    couldn't zoom in the browser using left ctrl + mouse scroll.

--- janus-key.c
+++ janus-key.c
@@ -1,5 +1,5 @@
 /*
-  
+
     janus-key. Give keys a double function.
 
     Copyright (C) 2021, 2022, 2023  Giulio Pietroiusti
@@ -23,11 +23,12 @@
 //  function.
 
 #include "./config.h"
-
 #include <assert.h>
+#include <bits/time.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/input.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -35,14 +36,16 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
-
 #include "libevdev/libevdev.h"
 #include "libevdev/libevdev-uinput.h"
+#include "poll.h"
+
+#define COUNTOF(x) (sizeof(x)/sizeof(*(x)))
+
+// max delay set by user stored as a timespec struct
+struct timespec delay_timespec;
 
 int last_input_was_special_combination = 0;
-
-// store delay value given by user into timespec struct
-struct timespec tp_max_delay;
 
 // For calculating delay
 struct timespec now;
@@ -51,8 +54,7 @@ struct timespec tp_sum;
 // If any of the janus keys is down or held return the index of the
 // first one of them in the mod_map. Otherwise, return -1.
 static int some_jk_are_down_or_held() {
-    size_t length = sizeof(mod_map)/sizeof(mod_map[0]);
-    for (int i = 0; i < length; i++) {
+    for (size_t i = 0; i < COUNTOF(mod_map); i++) {
         if (mod_map[i].state == 1 || mod_map[i].state == 2 && mod_map[i].secondary_function > 0)
             return i;
     }
@@ -62,8 +64,7 @@ static int some_jk_are_down_or_held() {
 // If `key` is in the mod_map, then return its index. Otherwise return
 // -1.
 static int is_in_mod_map(unsigned int key) {
-    size_t length = sizeof(mod_map)/sizeof(mod_map[0]);
-    for (int i = 0; i < length; i++) {
+    for (size_t i = 0; i < COUNTOF(mod_map); i++) {
         if (mod_map[i].key == key)
             return i;
     }
@@ -81,7 +82,7 @@ static int is_janus(unsigned int key) {
 }
 
 // Compare two timespec structs.
-// Return -1 if *tp1 < *tp2, 0 if *tp1 == *tp2, 1 if *tp1 < *tp2
+// Return -1 if *tp1 > *tp2, 0 if *tp1 == *tp2, 1 if *tp1 < *tp2
 static int timespec_cmp(struct timespec *tp1, struct timespec *tp2) {
     if (tp1->tv_sec > tp2->tv_sec) {
         return -1;
@@ -108,13 +109,35 @@ static void timespec_add(struct timespec* a, struct timespec* b, struct timespec
     }
 }
 
+// Subtracts timespec b from timespec a and stores the result in timespec c.
+// Assumes a >= b.
+static void timespec_subtract(const struct timespec* a, const struct timespec* b, struct timespec* c) {
+    c->tv_sec = a->tv_sec - b->tv_sec;
+    c->tv_nsec = a->tv_nsec - b->tv_nsec;
+    if (c->tv_nsec < 0) { // Underflow
+        c->tv_nsec += 1000000000;
+        c->tv_sec--;
+    }
+}
+
+// Converts a timespec struct to milliseconds.
+static long timespec_to_ms(const struct timespec *ts) {
+    long milliseconds = ts->tv_sec * 1000 + ts->tv_nsec / 1000000;
+    return milliseconds;
+}
+
+// Converts milliseconds to a timespec struct.
+static void ms_to_timespec(long ms, struct timespec *ts) {
+    ts->tv_sec = ms / 1000; // Convert milliseconds to seconds
+    ts->tv_nsec = (ms % 1000) * 1000000; // Convert the remainder to nanoseconds
+}
+
 // Post the EV_KEY event of code `code` and `value` through the
-// uninput device `*uidev` and send a (EV_SYN, SYN_REPORT, 0) event
+// uinput device `*uidev` and send a (EV_SYN, SYN_REPORT, 0) event
 // through that same device.
 static void send_key_ev_and_sync(const struct libevdev_uinput *uidev, unsigned int code, int value)
 {
     int err;
-
     err = libevdev_uinput_write_event(uidev, EV_KEY, code, value);
     if (err != 0) {
         perror("Error in writing EV_KEY event\n");
@@ -125,16 +148,19 @@ static void send_key_ev_and_sync(const struct libevdev_uinput *uidev, unsigned i
         perror("Error in writing EV_SYN, SYN_REPORT, 0.\n");
         exit(err);
     }
-    
     //printf("Sending %u %u\n", code, value);
 }
 
 // For each janus key down or held send an EV_KEY event with its
 // secondary function code and value `value`.
 static void send_down_or_held_jks_secondary_function(const struct libevdev_uinput *uidev, int value) {
-    for (int j = 0; j < sizeof(mod_map)/sizeof(mod_map[0]); j++) {
-        if (mod_map[j].state == 1 || mod_map[j].state == 2 && mod_map[j].secondary_function > 0)  {
-            send_key_ev_and_sync(uidev, mod_map[j].secondary_function, value);
+    for (size_t i = 0; i < COUNTOF(mod_map); i++) {
+        if ((mod_map[i].state == 1 || mod_map[i].state == 2) && mod_map[i].secondary_function > 0)  {
+            mod_map[i].delayed_down = 0;
+            if (mod_map[i].last_secondary_function_value_sent != value) {
+                send_key_ev_and_sync(uidev, mod_map[i].secondary_function, value);
+            }
+            mod_map[i].last_secondary_function_value_sent = value;
         }
     }
 }
@@ -159,16 +185,27 @@ static void handle_ev_key(const struct libevdev_uinput *uidev, unsigned int code
             jk->state = 1;
             clock_gettime(CLOCK_MONOTONIC, &jk->last_time_down);
             last_input_was_special_combination = 0;
+            // calculate time in which the delayed down event should
+            // happen (if relevant conditions are fulfilled)
+            struct timespec scheduled_delayed_down;
+            timespec_add(&jk->last_time_down, &delay_timespec, &scheduled_delayed_down);
+            jk->send_down_at = scheduled_delayed_down;
+            jk->delayed_down = 1;
         } else if (value == 2) {
-            jk->state = 1;
+            jk->state = 2;
             last_input_was_special_combination = 0;
         } else {
+            jk->delayed_down = 0;
             jk->state = 0;
             clock_gettime(CLOCK_MONOTONIC, &now);
-            timespec_add(&jk->last_time_down, &tp_max_delay, &tp_sum);
-            if (timespec_cmp(&now, &tp_sum) == 1) { // if now - jk->last_time_down < max_delay
+            //timespec_add(&jk->last_time_down, &tp_max_delay, &tp_sum);
+            timespec_add(&jk->last_time_down, &delay_timespec, &tp_sum);
+            if (timespec_cmp(&now, &tp_sum) == 1) { // is considered as tap
                 if (last_input_was_special_combination) {
-                    send_key_ev_and_sync(uidev, jk->secondary_function, 0);
+                    if (jk->last_secondary_function_value_sent != 0) {
+                        send_key_ev_and_sync(uidev, jk->secondary_function, 0);
+                    }
+                    jk->last_secondary_function_value_sent = 0;
                 } else {
                     if (some_jk_are_down_or_held() >= 0) {
                         last_input_was_special_combination = 1;
@@ -179,8 +216,11 @@ static void handle_ev_key(const struct libevdev_uinput *uidev, unsigned int code
                     send_primary_function(uidev, jk->key, 1);
                     send_primary_function(uidev, jk->key, 0);
                 }
-            } else {
-                send_key_ev_and_sync(uidev, jk->secondary_function, 0);
+            } else { // is not considered as tap
+                if (jk->last_secondary_function_value_sent != 0) {
+                    send_key_ev_and_sync(uidev, jk->secondary_function, 0);
+                }
+                jk->last_secondary_function_value_sent = 0;
             }
         }
     } else {
@@ -188,97 +228,160 @@ static void handle_ev_key(const struct libevdev_uinput *uidev, unsigned int code
             if (some_jk_are_down_or_held() >= 0) {
                 last_input_was_special_combination = 1;
                 send_down_or_held_jks_secondary_function(uidev, 1);
-                send_primary_function(uidev, code, 1);
             } else {
                 last_input_was_special_combination = 0;
-                send_primary_function(uidev, code, 1);
             }
+            send_primary_function(uidev, code, 1);
         } else if (value == 2) {
             if (some_jk_are_down_or_held() >= 0) {
                 last_input_was_special_combination = 1;
                 send_down_or_held_jks_secondary_function(uidev, 1);
-                send_primary_function(uidev, code, 1);
             } else {
                 last_input_was_special_combination = 0;
-                send_primary_function(uidev, code, 1);
             }
+            send_primary_function(uidev, code, 2);
         } else { // if (value == 0)
             send_primary_function(uidev, code, 0);
         }
     }
 }
 
-int
-main(int argc, char **argv)
-{
-    tp_max_delay.tv_sec = 0;
-    tp_max_delay.tv_nsec = max_delay * 1000000;
-
+int main(int argc, char **argv) {
+    ms_to_timespec(max_delay, &delay_timespec);
     struct libevdev *dev = NULL;
     const char *file;
-    int fd;
+    int read_fd;
     int rc = 1;
-
-    if (argc < 2)
-        goto out;
-
-    usleep(100000); // let (KEY_ENTER), value 0 go through before
-
+    if (argc < 2) {
+        exit(1);
+    }
+    usleep(100000); // let (KEY_ENTER), value 0 go through
     file = argv[1];
-    fd = open(file, O_RDONLY);
-    if (fd < 0) {
+    read_fd = open(file, O_RDONLY);
+    if (read_fd < 0) {
         perror("Failed to open device\n");
-        goto out;
+        exit(1);
     }
 
-    rc = libevdev_new_from_fd(fd, &dev);
+    // variables to manage timeout
+    struct input_event event;
+    unsigned got_event = 0; // if true then we have got an event, otherwise we have timed out
+    struct pollfd poll_fd;
+    poll_fd.fd = read_fd;
+    poll_fd.events = POLLIN;
+
+    rc = libevdev_new_from_fd(read_fd, &dev);
     if (rc < 0) {
         fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
-        goto out;
+        exit(1);
     }
 
     int err;
-    int uifd;
+    int write_fd;
     struct libevdev_uinput *uidev;
 
-    uifd = open("/dev/uinput", O_RDWR);
-    if (uifd < 0) {
+    write_fd = open("/dev/uinput", O_RDWR);
+    if (write_fd < 0) {
         printf("uifd < 0 (Do you have the right privileges?)\n");
         return -errno;
     }
 
-    err = libevdev_uinput_create_from_device(dev, uifd, &uidev);
+    err = libevdev_uinput_create_from_device(dev, write_fd, &uidev);
     if (err != 0)
         return err;
-        
+
     int grab = libevdev_grab(dev, LIBEVDEV_GRAB);
     if (grab < 0) {
         printf("grab < 0\n");
         return -errno;
     }
 
-    do {
-        struct input_event ev;
-        rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL|LIBEVDEV_READ_FLAG_BLOCKING, &ev);
-        if (rc == LIBEVDEV_READ_STATUS_SYNC) {
-            printf("janus_key: dropped\n");
-            while (rc == LIBEVDEV_READ_STATUS_SYNC) {
-                rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &ev);
+    while (rc == LIBEVDEV_READ_STATUS_SYNC || rc == LIBEVDEV_READ_STATUS_SUCCESS || rc == -EAGAIN) {
+        int has_pending_events = libevdev_has_event_pending(dev);
+        got_event = 0;
+        struct timespec timeout;
+
+        if (has_pending_events < 0) {
+            perror("pending");
+            exit(1);
+        }
+
+        if (has_pending_events == 1) {
+            got_event = 1;
+            rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL|LIBEVDEV_READ_FLAG_BLOCKING, &event);
+            if (rc == LIBEVDEV_READ_STATUS_SYNC) {
+                printf("janus_key: dropped\n");
+                while (rc == LIBEVDEV_READ_STATUS_SYNC) {
+                    rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &event);
+                }
+                printf("janus_key: re-synced\n");
             }
-            printf("janus_key: re-synced\n");
-        } else if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
-            if (ev.type == EV_KEY) {
-                handle_ev_key(uidev, ev.code, ev.value);
+        } else {
+            // find that mod_key whose `delayed_down` is true and has the
+            // soonest `send_down_at`, if any (there might not be a mod_key that
+            // satisfies those conditions)
+            int soonest_index = -1;
+            struct timespec soonest_val = {.tv_sec = 0, .tv_nsec = 0};
+            for (size_t i = 1; i < COUNTOF(mod_map); i++) {
+                if (mod_map[i].delayed_down && timespec_cmp(&mod_map[i].send_down_at, &soonest_val) == 1) {
+                    soonest_val = mod_map[i].send_down_at;
+                    soonest_index = i;
+                }
+            }
+
+            // decide whether to poll and calculate timeout
+            long poll_timeout = -1;
+            int should_poll = 0;
+            if (soonest_index == -1) {
+                // we should poll until a new event (second arg to poll will be -1)
+                should_poll = 1;
+            } else {
+                clock_gettime(CLOCK_MONOTONIC, &now);
+                if (timespec_cmp(&now, &(mod_map[soonest_index].send_down_at)) == 1) {
+                    should_poll = 1;
+                    timespec_subtract(&now, &(mod_map[soonest_index].send_down_at), &timeout);
+                }
+            }
+            if (should_poll && poll(&poll_fd, 1, poll_timeout)) {
+                got_event = 1;
+                rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL|LIBEVDEV_READ_FLAG_BLOCKING, &event);
+                if (rc == LIBEVDEV_READ_STATUS_SYNC) {
+                    printf("janus_key: dropped (after poll returned true)\n");
+                    while (rc == LIBEVDEV_READ_STATUS_SYNC) {
+                        rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &event);
+                    }
+                    printf("janus_key: re-synced (after poll returned true)\n");
+                }
             }
         }
-    } while (rc == LIBEVDEV_READ_STATUS_SYNC || rc == LIBEVDEV_READ_STATUS_SUCCESS || rc == -EAGAIN);
-    
+
+        // handle timers
+        for (size_t i = 0; i < COUNTOF(mod_map); i++) {
+            clock_gettime(CLOCK_MONOTONIC, &now);
+            timespec_subtract(&now, &(mod_map[i].send_down_at), &timeout);
+            int send_delay_down =  mod_map[i].delayed_down && timespec_cmp(&now, &(mod_map[i].send_down_at)) != 1;
+            if (send_delay_down) {
+                if (mod_map[i].last_secondary_function_value_sent != 1) {
+                    send_key_ev_and_sync(uidev, mod_map[i].secondary_function, 1);
+                }
+                mod_map[i].last_secondary_function_value_sent = 1;
+                mod_map[i].delayed_down = 0;
+            }
+        }
+
+        // handle new event if we have one
+        if (got_event && rc == LIBEVDEV_READ_STATUS_SUCCESS) {
+            if (event.type == EV_KEY) {
+                handle_ev_key(uidev, event.code, event.value);
+            }
+        }
+
+    }
+
     if (rc != LIBEVDEV_READ_STATUS_SUCCESS && rc != -EAGAIN)
         fprintf(stderr, "Failed to handle events: %s\n", strerror(-rc));
-    
-    rc = 0;
-out:
-    libevdev_free(dev);
-    
-    return rc;
+
+    // No need to use free memory (e.g., using libevdev_free) if the
+    // program is shutting down.
+    return 0;
 }
